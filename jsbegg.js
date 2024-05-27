579383c9b2db3286f9610e7ea49fa48afda033b8
goalkeeperindian <156797659+goalkeeperindian@users.noreply.github.com>
2024-05-27 17:35:18 +0530

    jsbegg.js

--- jsbegg.js
+++ jsbegg.js
@@ -1,42 +1,42 @@
 //We are here to create a variable to hold our NFT's
-const digitalAssets = []
+const miningNFTs = []
 // this function will accept values as arguments, construct an
 // NFT object using the provided arguments for its properties,
 // and add it to the array declared above.
-function createDigitalAsset (_owner, _eyeShade, _topWear, _accessory) {
+function createMiningNFT (_owner, _eyeShade, _topWear, _accessory) {
 
-    const digitalAsset = {
+    const miningNFT = {
         "owner": _owner,
         "eyeShade": _eyeShade,
         "topWear": _topWear,
         "accessory": _accessory
     }
-    digitalAssets.push(digitalAsset);
+    miningNFTs.push(miningNFT);
     console.log("Minted: "+ _owner);
 }
 
 // Lets create a "loop" that will pass through al the elements of an "array" of digital assets
 // and we try to print their metadata by using console.log()
-function displayDigitalAssets () {
-    for(i=0; i<digitalAssets.length; i++){
+function displayMiningNFTs () {
+    for(i=0; i<miningNFTs.length; i++){
         console.log("\nID: \t\t " +(i+1));
-        console.log("Name: \t\t "+digitalAssets[i].owner);
-        console.log("\nEye Color: \t "+digitalAssets[i].eyeShade);
-        console.log("\nShirt Type: "+digitalAssets[i].topWear);
-        console.log("\nBling: \t "+digitalAssets[i].accessory);
+        console.log("Name: \t\t "+miningNFTs[i].owner);
+        console.log("\nEye Color: \t "+miningNFTs[i].eyeShade);
+        console.log("\nShirt Type: "+miningNFTs[i].topWear);
+        console.log("\nBling: \t "+miningNFTs[i].accessory);
     }
 }
 
 //Here we are to print the total number of digital assets
-function totalDigitalAssets() {
-    console.log("\n"+digitalAssets.length);
+function totalMiningNFTs() {
+    console.log("\n"+miningNFTs.length);
 }
 
 // Let's Start calling our function
-createDigitalAsset("Lavish", "Black", "Footbal Jercy", "Classical Watch");
-createDigitalAsset("Nikhil", "Brown", "Shirts", "Locktet");
-createDigitalAsset("Jasanpreet", "Grey", "OnePiece", "Silver Braclet");
-createDigitalAsset("Vipula", "Black", "Crop top", "Hand Band");
-createDigitalAsset("Rahul Sain", "Green", "T-Shirt", "Kada");
-displayDigitalAssets();
-totalDigitalAssets();
+createMiningNFT("Lavish", "Black", "Footbal Jercy", "Classical Watch");
+createMiningNFT("Nikhil", "Brown", "Shirts", "Locktet");
+createMiningNFT("Jasanpreet", "Grey", "OnePiece", "Silver Braclet");
+createMiningNFT("Vipula", "Black", "Crop top", "Hand Band");
+createMiningNFT("Rahul Sain", "Green", "T-Shirt", "Kada");
+displayMiningNFTs();
+totalMiningNFTs();
